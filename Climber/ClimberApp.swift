f0ebc43dd6eb3b4925f9b4ef5e23caa0f23922d2
Alexander Shapiro <me@alexanderjshapiro.com>
2021-01-18 13:30:18 -0800

    Initialize project

---
+++ Climber/ClimberApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimberApp.swift
+//  Climber
+//
+//  Created by Alexander Shapiro on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ClimberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
